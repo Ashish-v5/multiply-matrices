a789c8147142de4a5644fe7275ef8dab65fdb372
Ashish7429 <ashish.11916604@lpu.in>
2023-01-25 11:37:31 +0530

    err

--- src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
+++ src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
@@ -2,11 +2,26 @@ package com.epam.rd.autotasks.matrices;
 import java.util.Arrays;
 
 public class MultiplyMatrix {
-    public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
+    public static int[][] multiply(int[][] mat1, int[][] mat2) {
+        int R1=mat1.length;
+        int C1=mat1[0].length;
+        int R2=mat2.length;
+        int C2=mat2[0].length;
 
-        // Put your code here
+        int[][] matrix1 = new int[R1][C2];
 
-        return null;
+        int i, j, k;
+        for (i = 0; i < R1; i++) {
+            for (j = 0; j < C2; j++) {
+                matrix1[i][j] = 0;
+                for (k = 0; k < R2; k++)
+                    matrix1[i][j] += mat1[i][k] * mat2[k][j];
+               // System.out.print(rslt[i][j] + " ");
+            }
+           // System.out.println("");
+        }
+
+        return matrix1;
     }
 
     public static void main(String[] args) {
